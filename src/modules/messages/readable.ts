977da12a81aace0497328ff184396e92b58fe4b2
Lasse Heitgres <hello@f-io.dev>
2025-06-07 06:31:50 +0000

    rename 'bitrate' to 'bitDepth' and add format/mimeType to AudioFormat

--- src/modules/messages/readable.ts
+++ src/modules/messages/readable.ts
@@ -147,7 +147,9 @@ export class Unplugged extends Message {
 export type AudioFormat = {
   frequency: 48000 | 44100 | 24000 | 16000 | 8000
   channel: 1 | 2
-  bitrate: number
+  bitDepth: number
+  format?:   string
+  mimeType?: string
 }
 
 type DecodeTypeMapping = {
@@ -158,37 +160,51 @@ export const decodeTypeMap: DecodeTypeMapping = {
   1: {
     frequency: 44100,
     channel: 2,
-    bitrate: 16,
+    bitDepth: 16,
+    format: "S16LE",
+    mimeType: "audio/L16; rate=44100; channels=2"
   },
   2: {
     frequency: 44100,
     channel: 2,
-    bitrate: 16,
+    bitDepth: 16,
+    format: "S16LE",
+    mimeType: "audio/L16; rate=44100; channels=2"
   },
   3: {
     frequency: 8000,
     channel: 1,
-    bitrate: 16,
+    bitDepth: 16,
+    format: "S16LE",
+    mimeType:  "audio/L16; rate=8000; channels=1"
   },
   4: {
     frequency: 48000,
     channel: 2,
-    bitrate: 16,
+    bitDepth: 16,
+    format: "S16LE",
+    mimeType:  "audio/L16; rate=48000; channels=2"
   },
   5: {
     frequency: 16000,
     channel: 1,
-    bitrate: 16,
+    bitDepth: 16,
+    format: "S16LE",
+    mimeType:  "audio/L16; rate=16000; channels=1"
   },
   6: {
     frequency: 24000,
     channel: 1,
-    bitrate: 16,
+    bitDepth: 16,
+    format: "S16LE",
+    mimeType:  "audio/L16; rate=24000; channels=1"
   },
   7: {
     frequency: 16000,
     channel: 2,
-    bitrate: 16,
+    bitDepth: 16,
+    format: "S16LE",
+    mimeType:  "audio/L16; rate=16000; channels=2"
   },
 }
 
